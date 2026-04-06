0e2ad8355df8d02c6ffe590e1be3f1eea50bffff
Pedro Renato Dos Santos Silva <pedro2002renato@gmail.com>
2026-04-06 16:48:33 -0300

    Add files via upload

---
+++ Projeto_Do_Aluno_Aprovado/Cálculo de média.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+nota3 = float(input("Digte a terceira nota:"))
+media = float((nota1 + nota2 + nota3) /3)
+print("A média do aluno é:", media)
+if media >= 7:
+    print("O aluno foi aprovado!")
+else:
+    print("O aluno foi reprovado!")
